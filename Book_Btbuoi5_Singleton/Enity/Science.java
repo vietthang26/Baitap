dd09c220d78e1542c3573e71de488ae5996d5e84
vietthang26 <90050791+vietthang26@users.noreply.github.com>
2021-10-04 17:52:32 +0700

    Book_Btbuoi5_Singleton
    Book_Btbuoi5_Singleton

---
+++ Book_Btbuoi5_Singleton/Enity/Science.java
@@ -0,0 +1,24 @@
+package Enity;
+
+public class Science extends Book {
+	private int xuatBan;
+
+	public Science(String nameTG, double price, int quantity, int xuatBan) {
+		super(nameTG, price, quantity);
+		this.xuatBan = xuatBan;
+	}
+
+	public int getXuatBan() {
+		return xuatBan;
+	}
+
+	public void setXuatBan(int xuatBan) {
+		this.xuatBan = xuatBan;
+	}
+
+	@Override
+	public String toString() {
+		return getId() + " | Tên tác giả: " + getNameTG() + " | Giá: " + getPrice() + " | Số lượng trong kho: "
+				+ getQuantity() + " | Xuất bản năm: " + getXuatBan();
+	}
+}
---
+++ Book_Btbuoi5_Singleton/Singleton/Inventory.java
@@ -0,0 +1,54 @@
+package Singleton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Enity.Book;
+
+
+public class Inventory {
+	static private List<Book> inventory = new ArrayList<Book>();;
+
+	private Inventory() {
+
+	}
+
+	static public List<Book> getInstance() {
+		return inventory;
+	}
+
+	// Thêm fruit vào inventory
+	static public boolean addToInventory(Book book) {
+		inventory.add(book);
+		return true;
+	}
+
+	/**
+	 * Kiểm tra id có tồn tại trong list hay không?
+	 * 
+	 * @param id
+	 * @return boolean
+	 */
+	static public boolean isValid(int id) {
+		for (Book i : inventory) {
+			if (i.getId() == id)
+				return true;
+		}
+		return false;
+	}
+
+	// Trả về Fruit dựa trên id
+	static public Book getBook(int id) {
+		return inventory.get(id - 1);
+	}
+	
+	static public boolean addQuantity(int id, int quantity) {
+		// Kiểm tra id có tồn tại hay không
+		if (Inventory.isValid(id) == false)
+			return false;
+		// Thêm số lượng
+		Book temp = Inventory.getBook(id);
+		temp.addQuantity(quantity);
+		return true;
+	}
+}
